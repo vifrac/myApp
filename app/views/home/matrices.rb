ed7ddb8907c3522e7a0465c8a1de18907b942106
vifrac <vifrac@gmail.com>
2019-10-18 00:04:14 -0500

    actualizacion rails 6.0.0

--- app/views/home/matrices.rb
+++ app/views/home/matrices.rb
@@ -21,3 +21,10 @@ video_30_curso_ruby = Video.new('objetos y clases')
 puts video_30_curso_ruby.title
 
 # prueba para ver los cambios de carga con ssh
+
+param1 = 1
+param2 = 2
+def add(*param1, param2)
+   return 3
+end
+puts add
